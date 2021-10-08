5548818273b02205c6890b95f1012fb99c70c00f
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2021-10-07 22:46:27 -0400

    BasicPlayerPlatforms
    Basic contents of lecture 4 added to project

--- 2D Platformer Game/Assets/Code/player.cs
+++ 2D Platformer Game/Assets/Code/player.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
+public int speed;
+    public int jumpForce = 500;
+    public int numJumps = 0;
+    Rigidbody2D _rigidbody;
+    public LayerMask groundLayer;
+    public Transform feet;
+    public bool onGround = false;
+    float xSpeed = 0;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        float xSpeed = Input.GetAxis("Horizontal") * speed;
+        _rigidbody.velocity = new Vector2(xSpeed, _rigidbody.velocity.y);
+
+    }
+
+    void Update(){
+        onGround = Physics2D.OverlapCircle(feet.position, .3f, groundLayer);
+        if(onGround){
+            numJumps = 1;
+        }
+    
+        if(Input.GetButtonDown("Jump") && (numJumps > 0 || onGround)){
+            // zeroing out the velocity makes sure that a double jump can't happen
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0);
+            _rigidbody.AddForce(new Vector2(0, jumpForce));
+            numJumps--;
+        }
     }
 }
