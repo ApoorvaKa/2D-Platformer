a8913c2dd2e563a588de2bdd605880f39863293c
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2021-10-19 02:26:28 -0400

    merged level 4, changed rock spawner, started slope stuff

--- 2D Platformer Game/Assets/Code/rock_spawn.cs
+++ 2D Platformer Game/Assets/Code/rock_spawn.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class rock_spawn : MonoBehaviour
 {   
     public GameObject rock;
-    
+    public int offset = 10;
+    public float time = 5;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("createRock", 10, 5);
+        InvokeRepeating("createRock", offset, time);
     }
 
     void createRock(){
